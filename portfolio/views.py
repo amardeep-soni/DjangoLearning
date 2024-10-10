56b630d9650d7219f768457844ac760345dad0f6
amardeep-soni <amardeepmunni@gmail.com>
2024-10-10 22:24:08 +0545

    passing datas from django view to template and using forLoop and  if else

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,15 +3,39 @@ from django.shortcuts import render
 
 
 def HomePage(request):
-    return render(request, "index.html")
+    data = {
+        "title": "Home",
+        "heading": "Welcome to My Portfolio",
+        "paragraph": "This is a simple portfolio template built using Django.",
+        "isActive": True,
+    }
+    return render(request, "index.html", data)
 
 
 def AboutPage(request):
     return render(request, "about.html")
 
 
+
 def ServicePage(request):
-    return render(request, "services.html")
+    data = {
+        "services": [
+            {
+                "title": "Service 1",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+                "title": "Service 2",
+                "description": "Nullam facilisis dolor ac neque convallis, at varius ligula condimentum.",
+            },
+            {
+                "title": "Service 3",
+                "description": "Ut tincidunt velit vel nunc pharetra, vel consectetur erat bibendum.",
+            },
+        ],
+    }
+    return render(request, "services.html", data)
+
 
 def ContactPage(request):
     return render(request, "contact.html")
