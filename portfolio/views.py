8da2c2038629b21c993b85c676104c0a19c3716d
amardeep-soni <amardeepmunni@gmail.com>
2024-10-12 17:29:54 +0545

    data passing removed currently

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,38 +3,15 @@ from django.shortcuts import render
 
 
 def HomePage(request):
-    data = {
-        "title": "Home",
-        "heading": "Welcome to My Portfolio",
-        "paragraph": "This is a simple portfolio template built using Django.",
-        "isActive": True,
-    }
-    return render(request, "index.html", data)
+    return render(request, "index.html")
 
 
 def AboutPage(request):
     return render(request, "about.html")
 
 
-
 def ServicePage(request):
-    data = {
-        "services": [
-            {
-                "title": "Service 1",
-                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            },
-            {
-                "title": "Service 2",
-                "description": "Nullam facilisis dolor ac neque convallis, at varius ligula condimentum.",
-            },
-            {
-                "title": "Service 3",
-                "description": "Ut tincidunt velit vel nunc pharetra, vel consectetur erat bibendum.",
-            },
-        ],
-    }
-    return render(request, "services.html", data)
+    return render(request, "services.html")
 
 
 def ContactPage(request):
